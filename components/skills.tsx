cfab806dfd45ed9b7d3477a113378999f50e54f2
colinguinane1 <colinguinane@gmail.com>
2024-10-25 19:34:30 -0600

    refactor: Update skills section with new backend skills and improve styling

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import FadeInSection from "./FadeInView";
-import { motion } from "framer-motion";
+import { color, motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { DiMongodb } from "react-icons/di";
 import { FaReact } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
 import { IoIosGitBranch } from "react-icons/io";
+import { IoLogoFirebase } from "react-icons/io5";
 import { RiNextjsFill } from "react-icons/ri";
 import { RiSupabaseFill } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
 import { SiTypescript } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
 
@@ -18,32 +20,13 @@ export default function Skills() {
 
   const darkMode = resolvedTheme === "dark";
   const iconSize = 25;
-  const skills = [
-    {
-      label: "TypeScript",
-      color: "blue",
-      icon: <SiTypescript size={iconSize} color="blue" />,
-      description: "JavaScript with type safety.",
-    },
-    {
-      label: "React",
-      color: "teal",
-      icon: <FaReact size={iconSize} color={"teal"} />,
-      description: "JavaScript Library",
-    },
+
+  const backendSkills = [
     {
       label: "Node.js",
       color: "green",
       icon: <FaNodeJs size={iconSize} color={"green"} />,
       description: "Server language",
-    },
-    {
-      label: "Next.js",
-      color: darkMode ? "white" : "black",
-      icon: (
-        <RiNextjsFill size={iconSize} color={darkMode ? "white" : "black"} />
-      ),
-      description: "React Framework",
     },
     {
       label: "Express",
@@ -68,6 +51,41 @@ export default function Skills() {
       color: "Chartreuse",
       icon: <RiSupabaseFill size={iconSize} color="Chartreuse" />,
       description: "Backend Service",
+    },
+    {
+      label: "Firebase",
+      color: "red",
+      icon: <IoLogoFirebase size={iconSize} color="red" />,
+      description: "Backend Service",
+    },
+  ];
+  const frontendSkills = [
+    {
+      label: "TypeScript",
+      color: "blue",
+      icon: <SiTypescript size={iconSize} color="blue" />,
+      description: "JavaScript with type safety.",
+    },
+    {
+      label: "React",
+      color: "teal",
+      icon: <FaReact size={iconSize} color={"teal"} />,
+      description: "JavaScript Library",
+    },
+
+    {
+      label: "Next.js",
+      color: darkMode ? "white" : "black",
+      icon: (
+        <RiNextjsFill size={iconSize} color={darkMode ? "white" : "black"} />
+      ),
+      description: "React Framework",
+    },
+    {
+      label: "Tailwind",
+      color: "cyan",
+      icon: <RiTailwindCssFill size={iconSize} color="cyan" />,
+      description: "Utility-first CSS",
     },
     {
       label: "Git",
@@ -95,7 +113,36 @@ export default function Skills() {
           initial="hidden"
           animate="visible"
         >
-          {skills.map((skill, index) => (
+          {frontendSkills.map((skill, index) => (
+            <motion.div
+              key={index}
+              variants={{
+                hidden: { opacity: 0, y: 20 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              <FadeInSection>
+                <div className="flex items-center gap-2 border bg-card p-3 rounded-md">
+                  <div
+                    style={{
+                      backgroundColor: skill.color,
+                    }}
+                    className="w-10 h-10 opacity-15 relative border items-center  justify-center flex rounded-md"
+                  ></div>{" "}
+                  <div className="absolute left-[21px] "> {skill.icon}</div>
+                  <div className="flex flex-col">
+                    <h1 className="font-bold tracking-tighter">
+                      {" "}
+                      {skill.label}
+                    </h1>
+                    <p className="hidden md:block">{skill.description}</p>
+                  </div>
+                </div>
+              </FadeInSection>
+            </motion.div>
+          ))}
+
+          {backendSkills.map((skill, index) => (
             <motion.div
               key={index}
               variants={{
