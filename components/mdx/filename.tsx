95923d5871a6ab321f1c0d63d0d54b2fa86d0193
colinguinane1 <colinguinane@gmail.com>
2024-10-26 01:34:50 -0600

    refactor: Add filename component and improve code snippet styling

--- components/mdx/filename.tsx
+++ components/mdx/filename.tsx
@@ -1,16 +1,21 @@
 export default function FileName({
   children,
-  fileName,
-  language = "jsx",
+  name,
+  language,
 }: {
   children: React.ReactNode;
-  fileName: string;
+  name: string;
   language?: string;
 }) {
   return (
-    <div className="bg-card w-full pt-2 rounded-md">
-      <div className="px-2 pb-1 flex justify-between">
-        <p>{fileName}</p>
+    <div className="bg-card w-full pt-2 border rounded-md">
+      <div className="px-2  pb-3 text-xs relative flex justify-between">
+        <div className="flex items-center gap-2 ">
+          <div className="bg-red-500/80 w-3 h-3 rounded-full "></div>
+          <div className="bg-yellow-500/80  w-3 h-3 rounded-full"></div>
+          <div className="bg-green-500/80  w-3 h-3 rounded-full"></div>
+        </div>
+        <p className=" absolute flex justify-center w-full">{name}</p>
         <p>{language}</p>
       </div>{" "}
       {children}
