f81668d665b36990520c17edb9496b4d9cec48b9
colinguinane1 <colinguinane@gmail.com>
2025-01-27 21:29:53 -0700

    feat: Integrate SyntaxHighlighter for code snippets in Viewport component

--- components/mdx/viewport.tsx
+++ components/mdx/viewport.tsx
@@ -1,5 +1,7 @@
 import { Tabs, TabsContent, TabsTrigger } from "../ui/tabs";
 import { TabsList } from "@radix-ui/react-tabs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export default function Viewport({
   children,
@@ -20,8 +22,10 @@ export default function Viewport({
         </div>
       </TabsContent>
       <TabsContent value="code">
-        <div className="">
-          <pre>{codeSnippet}</pre>
+        <div className="bg-card grid rounded-md place-content-center py-20 border">
+          <SyntaxHighlighter language="javascript" style={docco}>
+            {codeSnippet}
+          </SyntaxHighlighter>
         </div>
       </TabsContent>
     </Tabs>
