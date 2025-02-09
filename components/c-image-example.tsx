38e136a9fea33d84728e9dd4f557353bdf038a71
Colin Guinane <colinguinane@gmail.com>
2025-02-08 19:33:18 -0700

    Update c-image-example.tsx

--- components/c-image-example.tsx
+++ components/c-image-example.tsx
@@ -2,7 +2,7 @@ import CImage from "./c-image";
 
 const ImageExample = () => {
     return (
-        <CImage width={300} height={300} delay={2000} src="/example.jpg" />
+        <CImage width={300} height={300} delay={2000} src="https://indieground.net/wp-content/uploads/2023/03/Freebie-GradientTextures-Preview-05.jpg"/>
     )
 }
 
