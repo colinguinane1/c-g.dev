11c01d0ac87372f4ccc37eafd9b430f792ddc175
Colin Guinane <colinguinane@gmail.com>
2025-02-10 10:53:27 -0700

    Update c-image-example.tsx

--- components/c-image-example.tsx
+++ components/c-image-example.tsx
@@ -2,7 +2,7 @@ import CImage from "./c-image";
 
 const ImageExample = () => {
   return (
-    <CImage dropdown width={300} height={300} delay={2000} src="/example.jpg" />
+    <CImage loadingIndicator dropdown width={300} height={300} delay={2000} src="/example.jpg" />
   );
 };
 
