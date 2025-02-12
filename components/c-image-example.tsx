72f8e26c514de9ca65d0970e5564f64ab922e0a8
colinguinane1 <colinguinane@gmail.com>
2025-02-12 13:37:47 -0700

    Add dropdown prop to ImageExample component and implement loading indicator toggle

--- components/c-image-example.tsx
+++ components/c-image-example.tsx
@@ -1,10 +1,27 @@
-import CImage from "./c-image";
+"use client";
 
-const ImageExample = () => {
+import CImage from "./c-image";
+import { Switch } from "./ui/switch";
+import { Label } from "@radix-ui/react-label";
+import { useState } from "react";
+
+const ImageExample = (previewProp: string) => {
+  const props = [{ prop: "dropdown", default: false, label: "Dropdown" }];
+
+  const [propState, setPropState] = useState(false);
+
   return (
-    <CImage dropdown width={300} height={300} delay={2000} src="/example.jpg">
-      Example
-    </CImage>
+    <div className="mt-4">
+      <CImage dropdown width={300} height={300} delay={2000} src="/example.jpg">
+        Example
+      </CImage>
+      {props.find((p) => p.prop === previewProp) && (
+        <div className="flex items-center justify-center pt-4 space-x-2">
+          <Switch id="loading-indicator" />
+          <Label htmlFor="loading-indicator">Loading Indicator</Label>
+        </div>
+      )}
+    </div>
   );
 };
 
